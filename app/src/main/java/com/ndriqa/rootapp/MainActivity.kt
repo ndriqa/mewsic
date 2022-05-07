4b1b56fa9c8475d1732f09e56679f5caf0939ccb
Ndriçim Rrahmani <ndriqim.rrahmani@tenton.co>
2022-05-07 23:12:24 +0200

    fixed status bar

--- app/src/main/java/com/ndriqa/rootapp/MainActivity.kt
+++ app/src/main/java/com/ndriqa/rootapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ndriqa.rootapp
 
+import android.os.Build.*
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import com.ndriqa.rootapp.base.BaseActivity
@@ -10,20 +12,25 @@ class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (VERSION.SDK_INT >= VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initBaseFunctions()
     }
 
-    override fun onLoad() {}
+    override fun onLoad() {
+
+    }
 
     override fun setToolbar() {
-        binding.title.text = "Testione"
+        binding.toolbar.title.text = "Testione"
     }
 
     override fun onClickListeners() {
-        binding.menuButton.setOnClickListener {
+        binding.toolbar.menuButton.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.END)
         }
     }
